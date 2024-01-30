3e967ba0e30788b86ba1158a166c737b8929764d
Walid Saifi <saifiwalid21@gmail.com>
2024-01-30 11:51:18 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/about/{cat?}', function ($cat = null) {
+    return view('about', ['cat' => $cat]);
+});
+
