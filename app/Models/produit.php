6070c4f2847161b12628dbd27f625434c7219473
Walid Saifi <saifiwalid21@gmail.com>
2024-02-01 00:54:41 +0100

    Update produit.php

--- app/Models/produit.php
+++ app/Models/produit.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class produit extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'titre',
+        'description',
+        'prix',
+        'telephone',
+        'email'
+    ];
+
 }
