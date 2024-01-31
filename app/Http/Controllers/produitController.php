60160040fbefe8059718385475260018f24f30ac
Walid Saifi <saifiwalid21@gmail.com>
2024-02-01 00:54:21 +0100

    Update produitController.php

--- app/Http/Controllers/produitController.php
+++ app/Http/Controllers/produitController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\produit;
 use Illuminate\Http\Request;
 
 class produitController extends Controller
 {
     public  function index(){
-return view('produit/ajouterProduit');
+             return view('produit.index');
+    }
+    public  function create(){
+        return view( 'produit.create');
+
+    }
+    public function store(Request $request){
+
+              $data = $request->validate([
+                  'titre' => 'required ',
+                  'description' => 'required',
+                  'prix' => 'required',
+                  'telephone' => 'required',
+                  'email' => 'required | email'
+              ]);
+
+              $newProduit = produit::create($data);
+              return redirect(route('produit.index'));
+
     }
 }
